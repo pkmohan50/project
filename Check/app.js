f3d5e0e3792f4b0a89bf728a9fa059b2c03ae87c
pkmohan50 <pkmohan50@users.noreply.github.com>
2017-03-28 17:41:02 +0530

    Add files via upload

---
+++ Check/app.js
@@ -0,0 +1,38 @@
+		// Initialize Firebase
+	  	var config = {
+   		apiKey: "AIzaSyC1fK5BUmDK66Im40vesmMHadcjLseFxvQ",
+   		authDomain: "sih1-c04cf.firebaseapp.com",
+   		databaseURL: "https://sih1-c04cf.firebaseio.com",
+   		storageBucket: "sih1-c04cf.appspot.com",
+	  	messagingSenderId: "657409475726"
+		};
+		firebase.initializeApp(config);
+
+		var database = firebase.database();
+		var flyRef = database.ref('Flights').orderByChild('Time').startAt('13:50');
+		flyRef.on('value', gotData, errData);
+
+		function gotData(data){
+			//console.log(data.val());
+			var Flights = data.val();
+			var keys = Object.keys(Flights);
+			//console.log(keys);
+			for(var i = 0; i < keys.length; i++) {
+				var k = keys[i];
+				var Airlines = Flights[k].Airlines;
+				var Type = Flights[k].Type;
+				var Status=Flights[k].Status;
+				console.log(Airlines, Type, Status);
+				document
+					.querySelector("#flightlist")
+					.innerHTML+='<li class="list-group-item Flights[k]" style="border-style:solid; text-align:center; border-color:Teal; border-radius:50px; border-width:10px; font-size:30px; margin:20px; padding:10px 25px 10px 25px;  box-shadow: 0 0 20px 0 rgba(0, 0, 0, 0.2), 0 5px 5px 0 rgba(0, 0, 0, 0.24); text-align:center; position:relative; top:20px"<div  class="lead"><img style="position:relative; display:block; top:90px; text-align:right;" src="https://s15.postimg.org/aq8a956t7/462084.jpg" width="150" height="150"><p style="position:relative; bottom:75px; ">'+Flights[k].Airlines+'</p></div> <p style="position:relative; bottom:75px;">'+Flights[k].Type+'</p><p style="position:relative; bottom:75px;">'+Flights[k].Status+'</p></li>';
+				//var li=document.createElement('li',Airlines+":"+Type);
+				//$(li).parent('flightlist');
+				//documents.getElementById("type").innerHTML=Type;
+			}
+		}
+
+		function errData(err){
+			console.log("Error!");
+			console.log(err);
+		}
---
+++ Check/ini.js
@@ -0,0 +1,12 @@
+  		
+(function(){
+	// Initialize Firebase
+  	var config = {
+   	apiKey: "AIzaSyC1fK5BUmDK66Im40vesmMHadcjLseFxvQ",
+   	authDomain: "sih1-c04cf.firebaseapp.com",
+   	databaseURL: "https://sih1-c04cf.firebaseio.com",
+   	storageBucket: "sih1-c04cf.appspot.com",
+  	messagingSenderId: "657409475726"
+	};
+	firebase.initializeApp(config);
+}());
